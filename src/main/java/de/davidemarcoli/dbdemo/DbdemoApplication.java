d035338f1af60099e947becbeaa6854fcd1e6147
Davide Marcoli <davide@marcoli.ch>
2022-09-12 14:20:09 +0200

    Initial Commit

---
+++ src/main/java/de/davidemarcoli/dbdemo/DbdemoApplication.java
@@ -0,0 +1,13 @@
+package de.davidemarcoli.dbdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbdemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbdemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/davidemarcoli/dbdemo/DbdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package de.davidemarcoli.dbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbdemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
