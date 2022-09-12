0b75e0f859cff51099be5a796b4592d63ab812b5
Davide Marcoli <davide@marcoli.ch>
2022-09-12 18:14:23 +0200

    Add custom query

--- src/main/java/de/davidemarcoli/dbdemo/Person/PersonRepository.java
+++ src/main/java/de/davidemarcoli/dbdemo/Person/PersonRepository.java
@@ -2,8 +2,16 @@ package de.davidemarcoli.dbdemo.Person;
 
 import de.davidemarcoli.dbdemo.jpa.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PersonRepository extends JpaRepository<Person, Integer> {
+
+    // make a query that lists all persons with more than 10000$ in their account
+    @Query("SELECT p FROM Person p WHERE p.money > 10000")
+    List<Person> findAllRichPeople();
+
 }
